9034af4d81bdde9c0cbe9d830271a3ff32b58104
Fatima <fxtimaahmed14@gmail.com>
2022-10-18 14:20:16 +0100

    first card created?

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ console.log(data);
 //You can start simple and just render a single 
 //pokemon card from the first element
 console.log(data[0]);
+
+const ul = document.querySelector('ul')
+const li = document.createElement('li')
+
+li.setAttribute('class', 'card')
+ul.appendChild(li)
