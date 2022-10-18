8b3f4085434085cc3fd2d2b4668ca95b63851da4
MattVwaves <jamesmattholt@gmail.com>
2022-10-18 17:16:06 +0100

    all data displays correctly, but bullets remain

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 
 console.log(data);
-
-//You can start simple and just render a single 
-//pokemon card from the first element
 console.log(data[0]);
 
-const ul = document.querySelector('ul')
-const li = document.createElement('li')
+const cardsList = document.querySelector('ul')
 
-li.setAttribute('class', 'card')
-ul.appendChild(li)
+for (i = 0; i < data.length; i++){
+
+const pokemon = document.createElement('li')
+pokemon.setAttribute('class', 'card')
+cardsList.appendChild(pokemon)
+
+const h2 = document.createElement('h2')
+h2.innerText = data[i].name
+pokemon.appendChild(h2)
+
+const img = document.createElement('img')
+img.setAttribute('class', 'card--img')
+img.setAttribute('width', '256')
+img.src = data[i].sprites.other['official-artwork'].front_default
+pokemon.appendChild(img)
+
+const skillLevels = document.createElement('ul')
+skillLevels.setAttribute('class', 'card--text')
+pokemon.appendChild(skillLevels)
+
+for (j = 0; j < data[i].stats.length; j++){
+    const skill = document.createElement('li')
+    const name = data[i].stats[j].stat.name
+    const base_stat = data[i].stats[j].base_stat
+    skill.innerText = `${name}: ${base_stat}`
+    
+    skillLevels.appendChild(skill)}
+}
+
+
+
+
+
+
+
+// // const skillLevels = document.createElement('ul')
+// // skillLevels.setAttribute('class', 'card--text')
+// // pokemon.appendChild(skillLevels)
+
+// const skill = document.createElement('li')
+// skill.innerText = 'HP: 45'
+// skillLevels.appendChild(skill)
+
